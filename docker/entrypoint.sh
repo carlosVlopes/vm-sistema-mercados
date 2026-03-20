ec83b0717c8bbbd616666df3f7406a37c379a9a5
Carlos <dcl.carloslopes@gmail.com>
2026-03-19 21:27:47 -0300

    roolback migrate

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,6 @@ chmod -R 775 storage bootstrap/cache
 php artisan config:clear
 php artisan cache:clear
 php artisan config:cache
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
